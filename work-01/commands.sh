3406a5ed00627af00da39523df80e549849ebf44
М. Н. Шрайнер <wing.kiwi10@gmail.com>
2026-09-25 05:00:28 +0300

    Практика 1: дополнен журнал команд

--- work-01/commands.sh
+++ work-01/commands.sh
@@ -1,11 +1,46 @@
-export PREFIX=shrainer-06
-export ZONE=ru-central1-d
-export CIDR=10.16.1.0/24
+export PREFIX="shrainer-06"
+export ZONE="ru-central1-d"
+export CIDR="10.16.1.0/24"
+export DISK_SIZE="25"
+export IMAGE_FAMILY="ubuntu-2204-lts"
+export SSH_KEY="$HOME/.ssh/id_ed25519.pub"
 
-yc vpc network create --name "$PREFIX-net"
+yc vpc network create \
+  --name "$PREFIX-net"
 
 yc vpc subnet create \
   --name "$PREFIX-subnet" \
   --network-name "$PREFIX-net" \
   --zone "$ZONE" \
   --range "$CIDR"
+
+yc compute instance create \
+  --name "$PREFIX-app-1" \
+  --zone "$ZONE" \
+  --platform standard-v3 \
+  --cores=2 \
+  --core-fraction=20 \
+  --memory=2 \
+  --preemptible \
+  --create-boot-disk image-folder-id=standard-images,image-family="$IMAGE_FAMILY",type=network-hdd,size="$DISK_SIZE" \
+  --network-interface subnet-name="$PREFIX-subnet",nat-ip-version=ipv4 \
+  --ssh-key "$SSH_KEY" \
+  --labels created-by=script
+
+yc compute instance create \
+  --name "$PREFIX-app-2" \
+  --zone "$ZONE" \
+  --platform standard-v3 \
+  --cores=2 \
+  --core-fraction=20 \
+  --memory=2 \
+  --preemptible \
+  --create-boot-disk image-folder-id=standard-images,image-family="$IMAGE_FAMILY",type=network-hdd,size="$DISK_SIZE" \
+  --network-interface subnet-name="$PREFIX-subnet",nat-ip-version=ipv4 \
+  --ssh-key "$SSH_KEY" \
+  --labels created-by=script
+
+yc compute instance delete "$PREFIX-app-1"
+yc compute instance delete "$PREFIX-app-2"
+yc vpc subnet delete "$PREFIX-subnet"
+yc vpc network delete "$PREFIX-net"
